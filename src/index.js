e9d0ee5be33abac342f64625336cf056931ca7be
ARTH GAHLOD <149381333+Arth1803@users.noreply.github.com>
2024-10-18 02:43:11 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ProfileProvider } from './ProfileContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ProfileProvider>
+    <App />
+  </ProfileProvider>
+);
+
